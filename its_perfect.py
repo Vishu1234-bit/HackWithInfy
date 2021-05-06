0bd3eb2b98704cf54f064a8ffc1712560a100084
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-05-06 23:58:01 +0530

    Update its_perfect.py

--- its_perfect.py
+++ its_perfect.py
@@ -1,8 +1,13 @@
 n = int(input())
-f= []
-for i in range(1,n):
+f= set([1])
+if(n==1):
+    print("NO")
+i=2
+while(i*i<=n):
     if(n%i==0):
-        f.append(i)
+        f.add(i)
+        f.add(n//i)
+    i+=1
 if(sum(f)==n):
     print("yes")
 else:
